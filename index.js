b169e41e38ad218499a817c8d0a15b9fa275c89e
Tomi Javanainen <tomi.javanainen@gmail.com>
2020-06-16 19:13:12 +0300

    Notes as a standalone, Heroku-compatible app

---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+let notes = [ 
+    { 
+        id: 1, 
+        content: "HTML is easy - refresh test", 
+        date: "2020-01-10T17:30:31.098Z", 
+        important: true 
+    }, 
+    { 
+        id: 2, 
+        content: "Browser can execute only Javascript", 
+        date: "2020-01-10T18:39:34.091Z", 
+        important: false 
+    }, 
+    { 
+        id: 3, 
+        content: "GET and POST are the most important methods of HTTP protocol", 
+        date: "2020-01-10T19:20:14.298Z", 
+        important: true 
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World</h1>')
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    console.log(id)
+    const note = notes.find(note => note.id === id)
+    console.log(note)
+
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+})
+
+const generateId = () => {
+    const maxId = notes.length > 0
+    ? Math.max(...notes.map(note => note.id))
+    : 0
+    return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'Missing content'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date().toISOString(),
+        id: generateId()
+    }
+
+    console.log(note)
+
+    notes = notes.concat(note)
+    response.json(note)
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+    response.status(204).end()
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+
+const PORT = process.env.port || 3001
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
