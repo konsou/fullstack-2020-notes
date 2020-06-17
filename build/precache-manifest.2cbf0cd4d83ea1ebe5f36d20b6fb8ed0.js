4690bc97490d4acf5834b71ce9e6ba8620d6bcf7
Tomi Javanainen <tomi.javanainen@gmail.com>
2020-06-17 18:46:16 +0300

    Frontendin buildin lisäys

---
+++ build/precache-manifest.2cbf0cd4d83ea1ebe5f36d20b6fb8ed0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd6ae1b91dce551c8d87a76bb4f6e580",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29379e436948beb1e5a0",
+    "url": "/static/css/main.2892952e.chunk.css"
+  },
+  {
+    "revision": "07cfded20f7274abc421",
+    "url": "/static/js/2.08048b95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.08048b95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29379e436948beb1e5a0",
+    "url": "/static/js/main.c42a21ab.chunk.js"
+  },
+  {
+    "revision": "92137bbd888e9fcd6b75",
+    "url": "/static/js/runtime-main.e3d2f7d7.js"
+  }
+]);
