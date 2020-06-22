a39a82db728009cb74c393f5a3dee3a2ff69d079
Tomi Javanainen <tomi.javanainen@gmail.com>
2020-06-22 16:15:42 +0300

    dotenv-paketin lisäys

--- mongo.js
+++ mongo.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose')
 
+if (process.env.NODE_ENV !== 'production'){
+    const dotenv = require('dotenv')
+
+    dotenv.config()
+    console.log('env test: ', process.env.TEST)    
+}
+
 if (process.argv.length < 3){
     console.log('give password as argument')
     process.exit(1)
@@ -7,7 +14,7 @@ if (process.argv.length < 3){
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://fullstack-user:${password}@fullstack2020-bysw0.mongodb.net/test?retryWrites=true&w=majority`
+const url = `mongodb+srv://fullstack-user:${password}@fullstack2020-bysw0.mongodb.net/note-app?retryWrites=true&w=majority`
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -19,13 +26,20 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
+Note.find({ important: false }).then(result => {
+    result.forEach(note => console.log(note))
+    mongoose.connection.close()
+})
+/*
 const note = new Note({
-    content: 'HTML is easy',
+    content: 'This is the third test note',
     date: new Date(),
     important: true
 })
 
 note.save().then(response => {
-    console.log('note saved')
+    console.log('note saved:')
+    console.log(response)
     mongoose.connection.close()
 })
+*/
