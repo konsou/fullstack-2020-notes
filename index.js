53375c817fa133c4d98fdf2f885aab8cef56ed85
Tomi Javanainen <tomi.javanainen@gmail.com>
2020-06-29 19:15:44 +0300

    Remove unneeded default notes

--- index.js
+++ index.js
@@ -15,27 +15,6 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
 
-let notes = [ 
-    { 
-        id: 1, 
-        content: "HTML is easy - refresh test", 
-        date: "2020-01-10T17:30:31.098Z", 
-        important: true 
-    }, 
-    { 
-        id: 2, 
-        content: "Browser can execute only Javascript", 
-        date: "2020-01-10T18:39:34.091Z", 
-        important: false 
-    }, 
-    { 
-        id: 3, 
-        content: "GET and POST are the most important methods of HTTP protocol", 
-        date: "2020-01-10T19:20:14.298Z", 
-        important: true 
-    }
-]
-
 app.get('/', (request, response) => {
     response.send('<h1>Hello World</h1>')
 })
@@ -80,7 +59,7 @@ app.delete('/api/notes/:id', (request, response) => {
         .then(response2 => {
             console.log(`deleted ${response2.deletedCount} notes`)
             response2.deletedCount
-                ? response.status(200).send()
+                ? response.status(204).send()
                 : response.status(404).send()
         })
         .catch(error => {
